e1b5642e0f44f425f4da14c7d2ea223397e5a822
SSteers126 <sebastian.steers@live.com>
2021-01-11 01:59:48 +0000

    Basic driving
    The car now sports a (very basic) driving control system to actively all engine power to the rear wheels, apply brakes, or steer. (Control collisions are a known issue so will be worked on soon)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # Libraries for backend path conversion
 import sys,os
 
+from direct.showbase.InputStateGlobal import inputState
 from direct.showbase.ShowBaseGlobal import globalClock
 from direct.task.TaskManagerGlobal import taskMgr
 from panda3d.core import Filename
@@ -35,7 +36,8 @@ from direct.interval.IntervalGlobal import Sequence
 
 # Physics engine and other libraries for cars/maps
 from panda3d.core import Point3
-from panda3d.bullet import BulletBoxShape, BulletRigidBodyNode, BulletVehicle, BulletWorld, ZUp
+from panda3d.bullet import BulletDebugNode, BulletBoxShape, BulletRigidBodyNode, BulletVehicle, BulletWorld, ZUp, \
+    BulletPlaneShape
 from panda3d.core import TransformState
 
 # Threading similar to standard pythonic threading
@@ -43,6 +45,7 @@ from panda3d.core import Thread
 print(Thread.isThreadingSupported())
 
 # from gen.funcs import rel_path
+
 
 # Simple function to keep a value in a given range (by default 0 to 1)
 def clamp(i, mn=0, mx=1):
@@ -89,11 +92,68 @@ class MainWindow(ShowBase):
         wheel.setFrictionSlip(100.0)
         wheel.setRollInfluence(0.1)
 
+
+    def update(self, task):
+        dt = globalClock.getDt()
+        self.world.doPhysics(dt, 10, 0.008)
+        return task.cont
+
+    def accelerate(self):
+        self.vehicle.applyEngineForce(1000, 1)
+        self.vehicle.applyEngineForce(1000, 0)
+
+    def noaccelerate(self):
+        self.vehicle.applyEngineForce(0, 1)
+        self.vehicle.applyEngineForce(0, 0)
+
+    def brake(self):
+        self.vehicle.setBrake(100, 0)
+        self.vehicle.setBrake(100, 1)
+        self.vehicle.setBrake(100, 2)
+        self.vehicle.setBrake(100, 3)
+
+    def nobrake(self):
+        self.vehicle.setBrake(0, 0)
+        self.vehicle.setBrake(0, 1)
+        self.vehicle.setBrake(0, 2)
+        self.vehicle.setBrake(0, 3)
+
+    def turnleft(self):
+        self.steering += globalClock.getDt() * self.steeringIncrement
+        self.steering = min(self.steering, self.steeringClamp)
+        self.vehicle.setSteeringValue(self.steering, 2)
+        self.vehicle.setSteeringValue(self.steering, 3)
+
+
+    def turnright(self):
+        self.steering -= globalClock.getDt() * self.steeringIncrement
+        self.steering = max(self.steering, -self.steeringClamp)
+        self.vehicle.setSteeringValue(self.steering, 2)
+        self.vehicle.setSteeringValue(self.steering, 3)
+
     def __init__(self):
         # Initialize the ShowBase class from which we inherit, which will
         # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
         dir_path = Path(sys.path[0])
+
+        # Plane
+        self.worldNP = render.attachNewNode('World')
+        shape = BulletPlaneShape(Vec3(0, 0, 1), 0)
+
+        np = self.worldNP.attachNewNode(BulletRigidBodyNode('Ground'))
+        np.node().addShape(shape)
+        np.setPos(0, 0, -1)
+
+        # np.setCollideMask(BitMask32.allOn())
+
+        # Steering info
+        self.steering = 0.0  # degrees
+        self.steeringClamp = 45.0  # degrees
+        self.steeringIncrement = 120.0  # degrees per second
+
+        base.cam.setPos(0, -10, 4)
+        base.cam.lookAt(0, 0, 0)
 
         # Directional light 02
         directionalLight = DirectionalLight('directionalLight')
@@ -106,11 +166,16 @@ class MainWindow(ShowBase):
 
         self.lowPassFilter = AlphaTestAttrib.make(TransparencyAttrib.MDual, 0.5)
 
+        self.debugNP = self.worldNP.attachNewNode(BulletDebugNode('Debug'))
+        self.debugNP.show()
         self.world = BulletWorld()
+        self.world.setDebugNode(self.debugNP.node())
+        self.world.attachRigidBody(np.node())
+        self.world.setGravity(Vec3(0, 0, -9.81))
         # Chassis body
         # shape = BulletBoxShape(Vec3(0.7, 1.5, 0.5))
-        self.shape = BulletBoxShape(Vec3(1.65, 1.29, 4.0))
-        self.ts = TransformState.makePos(Point3(0, 0, 0.5))
+        self.shape = BulletBoxShape(Vec3(1, 2.2, 0.55))
+        self.ts = TransformState.makePos(Point3(0, 0, 0.75))
 
         self.chassisNP = render.attachNewNode(BulletRigidBodyNode('Car'))
         self.chassisNP.node().addShape(self.shape, self.ts)
@@ -121,59 +186,68 @@ class MainWindow(ShowBase):
         self.world.attachRigidBody(self.chassisNP.node())
 
         # Chassis geometry
-        loader.loadModel(self.rel_path("/src/models/cars/Supra Body ReScale.bam")).reparentTo(self.chassisNP)
+        loader.loadModel(self.rel_path("/src/models/cars/Supra Body ReScale rotate.bam")).reparentTo(self.chassisNP)
 
         # Vehicle
         self.vehicle = BulletVehicle(self.world, self.chassisNP.node())
         self.vehicle.setCoordinateSystem(ZUp)
         self.world.attachVehicle(self.vehicle)
 
-        self.LFwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel LF ReScale.bam"))
+        self.LFwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel L RS.bam"))
         self.LFwheelNP.reparentTo(render)
 
-        self.RFwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel RF ReScale.bam"))
+        self.RFwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel R RS.bam"))
         self.RFwheelNP.reparentTo(render)
 
-        self.LBwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel LB ReScale.bam"))
+        self.LBwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel L RS.bam"))
         self.LBwheelNP.reparentTo(render)
 
-        self.RBwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel RB ReScale.bam"))
+        self.RBwheelNP = loader.loadModel(self.rel_path("/src/models/cars/Supra Wheel R RS.bam"))
         self.RBwheelNP.reparentTo(render)
         # (0.79, -1.35, 0)
-        self.LFwheel = self.addWheel(Point3(0, 0, 0.35), True, self.LFwheelNP)
-        self.RFwheel = self.addWheel(Point3(0, 0, 0.35), True, self.RFwheelNP)
-        self.LBwheel = self.addWheel(Point3(0, 0, 0.35), False, self.LBwheelNP)
-        self.RBwheel = self.addWheel(Point3(0, 0, 0.35), False, self.RBwheelNP)
+        # Vec3(1, 2.2, 0.55)
+        self.LFwheel = self.addWheel(Point3(0.9, -1.35, 0.65), True, self.LFwheelNP)
+        self.RFwheel = self.addWheel(Point3(-0.9, -1.35, 0.65), True, self.RFwheelNP)
+        self.LBwheel = self.addWheel(Point3(0.9, 1.35, 0.65), False, self.LBwheelNP)
+        self.RBwheel = self.addWheel(Point3(-0.9, 1.35, 0.65), False, self.RBwheelNP)
+
+        # debugNode = BulletDebugNode('Debug')
+        # debugNode.showWireframe(True)
+        # debugNode.showConstraints(True)
+        # debugNode.showBoundingBoxes(False)
+        # debugNode.showNormals(False)
+        # debugNP = render.attachNewNode(debugNode)
+        # debugNP.show()
+
+        # world = BulletWorld()
+        # world.setGravity(Vec3(0, 0, -9.81))
+        # self.world.setDebugNode(debugNP.node())
 
         ## TODO: coord system - xpositive = right, ypositive = back, zpositive = up
-        # self.LFwheel = self.vehicle.createWheel()
-        # self.LFwheel.setNode(self.LFwheelNP.node())
-        # self.LFwheel.setChassisConnectionPointCs(Point3(0, 0, 0.4))
-        # self.LFwheel.setFrontWheel(True)
-        #
-        # self.LFwheel.setWheelDirectionCs(Vec3(0, 0, -1))
-        # self.LFwheel.setWheelAxleCs(Vec3(1, 0, 0))
-        # self.LFwheel.setWheelRadius(0.25)
-        # self.LFwheel.setMaxSuspensionTravelCm(10.0)
-        #
-        # self.LFwheel.setSuspensionStiffness(40.0)
-        # self.LFwheel.setWheelsDampingRelaxation(2.3)
-        # self.LFwheel.setWheelsDampingCompression(4.4)
-        # self.LFwheel.setFrictionSlip(100.0)
-        # self.LFwheel.setRollInfluence(0.1)
+
+        self.accept("w-repeat", self.accelerate)
+        self.accept("w", self.accelerate)
+        self.accept("w-up", self.noaccelerate)
+        self.accept("s-repeat", self.brake)
+        self.accept("s", self.brake)
+        self.accept("s-up", self.nobrake)
+        self.accept("a-repeat", self.turnleft)
+        self.accept("a", self.turnleft)
+        # self.accept("a-up", self.nobrake)
+        self.accept("d-repeat", self.turnright)
+        self.accept("d", self.turnright)
+        # self.accept("d-up", self.nobrake)
 
         # Use a 512x512 resolution shadow map
         directionalLight.setShadowCaster(True, 512, 512)
         # Enable the shader generator for the receiving nodes
         render.setShaderAuto()
 
-        # Update
-        def update(task):
-            dt = globalClock.getDt()
-            self.world.doPhysics(dt)
-            return task.cont
 
-        taskMgr.add(update, 'update')
+        # Update
+
+
+        taskMgr.add(self.update, 'update')
 
 
 if __name__ == '__main__':
